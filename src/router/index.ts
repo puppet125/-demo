1d0ef14298414f7b2f923421b07137dbf41bc5b7
puppetzh <puppet125>
2022-07-18 14:04:17 +0800

    初始化

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/home",
+    name: "Home",
+    component: () => import("@/views/home.vue"),
+  },
+  {
+    path: "/",
+    redirect: { name: "Home" },
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+export default router;
---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+const add = () => {
+  count.value++;
+  console.log(count);
+};
+</script>
+<template>
+  <div>
+    <button>{{'count:'count}}</button>
+  </div>
+</template>
+<style scoped lang="less"></style>
